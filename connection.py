cdfe0f663814fc691fd5626af64561348a113aa4
RobertJunghanns <51117160+RobertJunghanns@users.noreply.github.com>
2023-06-27 19:07:37 +0200

    tried connection to activeMQ

---
+++ connection.py
@@ -0,0 +1,42 @@
+import time 
+import sys
+import stomp
+
+class Listener(stomp.ConnectionListener):
+    def on_error(self, headers, message):
+        print('received an error "%s"' % message)
+    def on_message(self, headers, message):
+        print('received a message "%s"' % message)
+
+class activeMqNode:
+    def __init__(self, hostPort, id, queryTopic, inputTopics): 
+        hosts = [('localhost', hostPort)] 
+        conn = stomp.Connection(host_and_ports=hosts)
+        conn.set_listener(id, Listener())
+        #conn.start()
+        conn.connect('admin', 'admin', wait=True)
+
+        for topic in inputTopics:
+            conn.subscribe(destination='/topic/'+topic, ack='auto') 
+
+        self.conn = conn
+        self.id = id
+        self.queryTopic = queryTopic
+        self.inputTopics = inputTopics
+    
+    def send(self, message):
+        self.conn.send(body=message, destination='/topic/'+self.queryTopic)
+    
+    def disconnect(self):
+        self.conn.disconnect()
+
+    def __str__(self):
+        return f"Connection(conn={self.conn}, id='{self.id}', queryTopic='{self.queryTopic}', inputTopics={self.inputTopics})"
+    
+
+
+    
+    
+    
+
+    
